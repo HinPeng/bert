09f3e6582db4c549f2abbb8c1e0ae53e9e64abbf
shenhuan <shenhuan1996@qq.com>
2019-11-20 08:47:26 -0500

    fix

--- run_bert.sh
+++ run_bert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BERT_BASE_DIR=/home/frog/maweiliang/models/bert-master/bert_models/bert_base_uncase/
+export BERT_BASE_DIR=$HOME/vfonel/bert/bert_base_uncase/
 # export BERT_LARGE_DIR=/home/frog/maweiliang/models/bert-master/bert_models/bert_large_uncase/
 rm -r ./output/pretraining_output
 # export TF_LOG_TENSOR_INFO=true
@@ -15,7 +15,7 @@ export TF_MODEL_NUM_NODES=7349 # bs=70
 # export SWAP_POLICY_FILE=/mnt/xq/TFCompGraphSim_backup/bert_66_p100/swapping_decision_ondemand_16.log
 # export SWAP_POLICY_FILE=/mnt/vfonel/backup/TFCompGraphSim/bert_66_p100/swapping_decision.log
 
-export RECOMPUTE_POLICY_FILE=/home/frog/maweiliang/work/TFCompGraphSim/bert_66_p100/recompute_400.log
+# export RECOMPUTE_POLICY_FILE=/home/frog/maweiliang/work/TFCompGraphSim/bert_66_p100/recompute_400.log
 # export RECOMPUTE_POLICY_FILE=/home/frog/maweiliang/tmp/TFCompGraphSim/bertlarge_8_p100/recompute.log
 # export RECOMPUTE_POLICY_FILE=/home/frog/maweiliang/tmp/TFCompGraphSim/bertlarge_8_p100/recompute_only_once.log
 # export RECOMPUTE_POLICY_FILE=/mnt/maweiliang/work/TFCompGraphSim/bert_66_p100/recompute_d0_70.log
@@ -34,6 +34,7 @@ python run_pretraining.py \
   --num_warmup_steps=10 \
   --learning_rate=2e-5 \
   --save_checkpoints_steps=0 \
+  --allow_growth=False
 #  --lognode_time=True \
 #  --openai_opt=True \
 #  --memory_optimization=SWAPPING_HEURISTICS
